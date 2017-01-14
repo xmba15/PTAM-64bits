b4c1f7368a31c7e84d3311cf71b6011c8a2bac21
bajsk <tran@jsk.imi.i.u-tokyo.ac.jp>
2017-01-14 11:16:49 +0900

    change videosource header

--- VideoSource_backup.h
+++ VideoSource_backup.h
@@ -1,8 +1,17 @@
-// This VideoSource for Win32 uses EWCLIB
+// -*- c++ *--
+// Copyright 2008 Isis Innovation Limited
 //
-// EWCLIB ver.1.2
-// http://www.geocities.jp/in_subaru/ewclib/index.html
-
+// VideoSource.h
+// Declares the VideoSource class
+// 
+// This is a very simple class to provide video input; this can be
+// replaced with whatever form of video input that is needed.  It
+// should open the video input on construction, and provide two
+// function calls after construction: Size() must return the video
+// format as an ImageRef, and GetAndFillFrameBWandRGB should wait for
+// a new frame and then overwrite the passed-as-reference images with
+// GreyScale and Colour versions of the new frame.
+//
 #include <cvd/image.h>
 #include <cvd/byte.h>
 #include <cvd/rgb.h>
@@ -11,13 +20,12 @@ struct VideoSourceData;
 
 class VideoSource
 {
-public:
-	VideoSource();
-	~VideoSource();
-	void GetAndFillFrameBWandRGB(CVD::Image<CVD::byte> &imBW, CVD::Image<CVD::Rgb<CVD::byte> > &imRGB);
-	CVD::ImageRef Size();
-
-private:
-	unsigned char *m_buffer;
-	CVD::ImageRef mirSize;
+ public:
+  VideoSource();
+  void GetAndFillFrameBWandRGB(CVD::Image<CVD::byte> &imBW, CVD::Image<CVD::Rgb<CVD::byte> > &imRGB);
+  CVD::ImageRef Size();
+  
+ private:
+  void *mptr;
+  CVD::ImageRef mirSize;
 };
